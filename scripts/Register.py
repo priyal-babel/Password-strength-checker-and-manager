366732241c47da27d1ab4f4e5b68556cda9a7fae
Hinal Pujara <hinal.pujara@somaiya.edu>
2022-02-17 00:03:36 +0530

    Specification Changed

--- scripts/Register.py
+++ scripts/Register.py
@@ -62,10 +62,10 @@ class Register:
             x = 0
             policy = PasswordPolicy.from_names(
                 length=8,  # min length: 8
-                uppercase=2,  # need min. 2 uppercase letters
-                numbers=2,  # need min. 2 digits
-                special=2,  # need min. 2 special characters
-                nonletters=2,  # need min. 2 non-letter characters (digits, specials, anything)
+                uppercase=1,  # need min. 2 uppercase letters
+                numbers=1,  # need min. 2 digits
+                special=1,  # need min. 2 special characters
+                nonletters=1,  # need min. 2 non-letter characters (digits, specials, anything)
             )
             policy.test(passw)
             if policy.test(passw) == []:
